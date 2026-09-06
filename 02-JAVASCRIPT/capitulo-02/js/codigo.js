c7437234c7e6a08a305e59e8f8e26f0d8417d643
Eduardo <dj.shadowjeab@gmail.com>
2026-09-05 20:28:32 -0500

    e

--- 02-JAVASCRIPT/capitulo-02/js/codigo.js
+++ 02-JAVASCRIPT/capitulo-02/js/codigo.js
@@ -18,3 +18,26 @@ btn.addEventListener('click', function(){
 
 
 // 4️⃣ eliminar tareas al hacer click
+const listaTareas = document.querySelectorAll('.item');
+// console.log(listaTareas.length);
+// for (let i = 0; i < listaTareas.length; i++) {
+//     console.log(listaTareas[i]);
+// }
+
+// METODO FOREACH
+// listaTareas.forEach(function(item){
+//     // console.log(item)
+//     item.addEventListener('click', function() {
+//         // console.log('hiciste clik en un item');
+//         item.remove();
+//     })
+// });
+
+const listaBox = document.querySelector('ul');
+// console.log(listaBox);
+listaBox.addEventListener('click', function(e){
+    // console.log(e);
+    if(e.target.tagName === 'LI') {
+        e.target.remove();
+    }
+});
