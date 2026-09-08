f668dd144b4a798d05a262b578ceacf438b951b9
Eduardo <dj.shadowjeab@gmail.com>
2026-09-07 22:42:00 -0500

    c5

---
+++ 02-JAVASCRIPT/capitulo-05/js/conceptos.js
@@ -0,0 +1,13 @@
+// ⚡⚡ EJECUCIONES SINCRONAS Y ASINCRONAS ⚡⚡
+
+// 1️⃣ SINCRONO
+let num = 10;
+console.log(num);
+
+// asincrona
+
+function saludar() {
+    console.log('hola a todos');
+}
+
+saludar()
